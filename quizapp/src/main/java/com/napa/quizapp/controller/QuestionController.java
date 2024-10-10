1efb1101a5d8b79f1484e7976b39253550bae272
Prasanth <napaprasanth18@gmail.com>
2024-10-10 22:16:53 +0530

    pagination changes

--- quizapp/src/main/java/com/napa/quizapp/controller/QuestionController.java
+++ quizapp/src/main/java/com/napa/quizapp/controller/QuestionController.java
@@ -3,10 +3,13 @@ package com.napa.quizapp.controller;
 import com.napa.quizapp.model.Question;
 import com.napa.quizapp.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("question")
@@ -23,6 +26,21 @@ public class QuestionController {
     @GetMapping("category/{category}")
     public ResponseEntity<List<Question>> getQuestionsByCategory(@PathVariable String category){
         return questionService.getQuestionsByCategory(category);
+    }
+
+    @GetMapping
+    public ResponseEntity<Map<String, Object>> getQuestions(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size) {
+        Page<Question> questions = questionService.getPaginatedQuestions(page, size);
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("questions", questions.getContent());
+        response.put("currentPage", questions.getNumber());
+        response.put("totalItems", questions.getTotalElements());
+        response.put("totalPages", questions.getTotalPages());
+
+        return ResponseEntity.ok(response);
     }
 
     @PostMapping("add")
--- quizapp/src/main/java/com/napa/quizapp/service/QuestionService.java
+++ quizapp/src/main/java/com/napa/quizapp/service/QuestionService.java
@@ -3,6 +3,9 @@ package com.napa.quizapp.service;
 import com.napa.quizapp.model.Question;
 import com.napa.quizapp.dao.QuestionDao;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -32,6 +35,11 @@ public class QuestionService {
         }
         return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
 
+    }
+
+    public Page<Question> getPaginatedQuestions(int page, int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        return questionDao.findAll(pageable);
     }
 
     public ResponseEntity<String> addQuestion(Question question) {
